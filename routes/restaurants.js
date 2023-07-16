450095cdae15e4abcca17b15f0e135a8706a93d7
peipeipeipeigit <szupei21@gmail.com>
2023-07-17 03:49:22 +0800

    refactor routes/restaurant.js

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,9 +2,44 @@ const express = require('express')
 const { route } = require('.')
 const router = express.Router()
 
+const Restaurant = require('../models/restaurant')
 
 
+// post a restaurant
+router.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+router.post('/restaurants', (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
 
+
+// edit a restaurant
+router.get('/restaurants/:restaurant_id/edit', (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findById(restaurantId)
+    .lean()
+    .then(restaurants => res.render('edit', { restaurants }))
+    .catch(err => console.log(err))
+})
+
+router.put('/restaurants/:restaurant_id', (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findByIdAndUpdate(restaurantId, req.body)
+    .then(() => res.redirect(`/restaurants/${restaurantId}`))
+    .catch(err => console.log(err))
+})
+
+
+// delete a restaurant
+router.delete('/restaurants/:restaurant_id', (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findByIdAndDelete(restaurantId)
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
 
 
 
