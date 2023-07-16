32dceff5a8e4c9743476ff93fa5c385b40bd99b5
peipeipeipeigit <szupei21@gmail.com>
2023-07-17 03:05:25 +0800

    refactor app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-//基礎建設：express及template engine及port
+// basic setting
 const express = require('express')
-const app = express()
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 const exphbs = require('express-handlebars')
-const port = 3000
+const app = express()
+const port = process.env.PORT || 3000
+const routes = require('./routes')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// require to connect database
+require('./config/mongoose.js')
 
 //基礎文件：載入json文件
 const restaurantList = require('./restaurant.json')
@@ -39,5 +50,5 @@ app.get('/search', (req, res) => {
 
 // 啟動伺服器並監聽
 app.listen(port, () => {
-  console.log(`app.js listening on http://localhost:${port}`)
+  console.log(`app.js is listening on http://localhost:${port}`)
 })
