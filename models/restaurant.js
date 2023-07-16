0236c5b78613982432b26ac73859ae7394f406f7
peipeipeipeigit <szupei21@gmail.com>
2023-07-17 04:25:17 +0800

    feat: add models/restaurant.js

---
+++ models/restaurant.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  name: {
+    type: String, 
+    required: true 
+  },
+  name_en: {
+    type: String, 
+    required: true 
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String,
+    required: true
+  },
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
