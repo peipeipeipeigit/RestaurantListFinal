6ca33d4d32296d462ff406b109a3562b3b27d3e5
peipeipeipeigit <szupei21@gmail.com>
2023-07-17 16:27:09 +0800

    feat: add restaurantSeeder.js

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,47 @@
+// 連線設定
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const db = require('../../config/mongoose')
+
+// require restaurant model
+const Restaurant = require('..restaurant.js')
+const User = require('../user')
+const restaurantData = require('../../restaurant.json')
+
+const SEED_USER = {
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678'
+}
+
+db.once('open', () => {
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      const { id, name, name_en, category, rating } = restaurantData.results
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Restaurant.create({
+          id,
+          name,
+          name_en,
+          category,
+          rating
+        })
+      )
+      )
+        .then(() => {
+          console.log('done.')
+          process.exit()
+        })
+    })
+})
